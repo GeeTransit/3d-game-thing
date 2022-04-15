189dd9092ca36533d92214f3fe377f2e6d5d8d77
George Zhang <geetransit@gmail.com>
2022-04-15 13:17:36 -0400

    Make Vector class import lazy

--- Point.py
+++ Point.py
@@ -4,7 +4,7 @@ from copy import copy
 from dataclasses import dataclass
 from warnings import warn
 
-from Vector import Vector
+import Vector
 
 @dataclass
 class Point:
@@ -27,22 +27,22 @@ class Point:
     def getMidpoint(p: Point, q: Point) -> Point:
         return Point((p.x+q.x)//2, (p.y+q.y)//2, (p.z+q.z)//2)
 
-    def __iadd__(self, other: Vector) -> Point:
+    def __iadd__(self, other: Vector.Vector) -> Point:
         self.x += int(other.x)
         self.y += int(other.y)
         self.z += int(other.z)
         return self
-    def __add__(self, other: Vector) -> Point:
+    def __add__(self, other: Vector.Vector) -> Point:
         new = copy(self)
         new += other
         return new
 
-    def __isub__(self, other: Vector) -> Point:
+    def __isub__(self, other: Vector.Vector) -> Point:
         self.x -= int(other.x)
         self.y -= int(other.y)
         self.z -= int(other.z)
         return self
-    def __sub__(self, other: Vector) -> Point:
+    def __sub__(self, other: Vector.Vector) -> Point:
         new = copy(self)
         new -= other
         return new
